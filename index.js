fa038e0df61ef43e04c1d2e1620f57c35cae9b96
abel-tefera <abeltefera16@gmail.com>
2023-03-07 11:15:02 +0300

    mobile menu done

--- index.js
+++ index.js
@@ -6,5 +6,12 @@
        closemenu.style.visibility = 'visible';
        closemenu.style.height = '100%';
     } 
+    else if (element.style.display == 'none') {
+        closemenu.style.height = '0';
+        setTimeout(() => {
+            closemenu.style.visibility = 'hidden';
+            element.style.display = 'block';
+           }, 200);
+    }
 }
     
