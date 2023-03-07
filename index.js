bb53e6e2ceec7f8d3901649f13defd1f1f33c860
vikas924 <obroy902@gmail.com>
2023-03-07 15:32:43 +0530

    fix linters

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
- function showhide(){
-    let element = document.querySelector('.button');
-    let closemenu = document.querySelector('.mobile-menu')
-    if (element.style.display != 'none') {
-            element.style.display = 'none';
-       closemenu.style.visibility = 'visible';
-       closemenu.style.height = '100%';
-    } 
-    else if (element.style.display == 'none') {
-        closemenu.style.height = '0';
-        setTimeout(() => {
-            closemenu.style.visibility = 'hidden';
-            element.style.display = 'block';
-           }, 200);
-    }
+function showhide() {
+  const element = document.querySelector('.button');
+  const closemenu = document.querySelector('.mobile-menu');
+  if (element.style.display !== 'none') {
+    element.style.display = 'none';
+    closemenu.style.visibility = 'visible';
+    closemenu.style.height = '100%';
+  } else if (element.style.display === 'none') {
+    closemenu.style.height = '0';
+    setTimeout(() => {
+      closemenu.style.visibility = 'hidden';
+      element.style.display = 'block';
+    }, 200);
+  }
 }
-    
+
+const openMenu = document.querySelector('.button');
+openMenu.addEventListener('click', showhide);
+
+const closeMenu = document.querySelector('.mobile-menu');
+closeMenu.addEventListener('click', showhide);
